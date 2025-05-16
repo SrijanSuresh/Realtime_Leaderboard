6457ded10193697097dc65e983068f65ea50efab
SrijanSuresh <126934224+SrijanSuresh@users.noreply.github.com>
2025-05-16 11:02:53 -0500

    updated layout and page

---
+++ realtime-leaderboard/app/page.jsx
@@ -0,0 +1,10 @@
+import Leaderboard from './components/Leaderboard'
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Realtime Leaderboard</h1>
+      <Leaderboard />
+    </main>
+  )
+}
